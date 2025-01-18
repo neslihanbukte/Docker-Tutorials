33ee5613aa08c17bab8fd9897bbf12acf8d9b0b5
Neslihan Bükte <neslihanbukte20@gmail.com>
2025-01-18 19:49:56 +0300

    Update Tech.js file

--- Dockerfile_data/Tech.js
+++ Dockerfile_data/Tech.js
@@ -1,16 +1,13 @@
-// Tech.js 
-const express= require('express');
-const app= express();
-const port=9999;
+const express = require('express');
+const app = express();
+const port = 9999;
 
-// GET 
+// GET
 app.get('/', (req, res) => {
-    res.send('Dockerfile Node js Çalıştı');
-    res.setHeader('Content-Type', 'text/plain');
-    res.statusCode=200;
+    res.status(200).send('Dockerfile Node js Çalıştı.');
 });
 
 // LISTEN
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Tech js app listening Server http://localhost:${port} portunda çalışıyor`);
-})
+});
